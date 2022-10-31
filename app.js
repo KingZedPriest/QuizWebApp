45af443acb7d0af1dc0c40ceb0f65f350cef27db
ZedPriest <Charleschukwuemeka47@gmail.com>
2022-10-31 16:37:14 +0100

    Add questions and answers

--- app.js
+++ app.js
@@ -184,3 +184,25 @@ let questions = [
     answer: 3,
   },
 ];
+//Needed Constants
+const CORRECT_BONUS = 10; //This is the worth of the Correct Answers if chosen.
+const MAX_QUESTIONS = 3; //The Total number of Questions a user gets.
+
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  console.log(availableQuestions);
+  getNewQuestion();
+};
+getNewQuestion = () => {
+  questionCounter++;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.innerText = currentQuestion.question;
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerHTML = currentQuestion["choice" + number];
+  })
+};
+startGame();
